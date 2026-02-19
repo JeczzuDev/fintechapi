5915eaef64662870f462d76287122b4b4889b31f
JeczzuDev <jesusignaciocc22@gmail.com>
2026-02-19 11:49:13 -0300

    Initialize Fintech API project with Maven setup, application configuration, and basic structure

---
+++ src/main/java/com/jeczzu/fintechapi/FintechapiApplication.java
@@ -0,0 +1,13 @@
+package com.jeczzu.fintechapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeczzu/fintechapi/FintechapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeczzu.fintechapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
