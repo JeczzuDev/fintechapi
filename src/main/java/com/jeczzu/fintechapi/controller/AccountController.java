024584e1471de3022b065ba9828299c4c74331ec
JeczzuDev <jesusignaciocc22@gmail.com>
2026-02-20 13:00:51 -0300

    feat(account): Add controller with endpoints to create and get account

---
+++ src/main/java/com/jeczzu/fintechapi/controller/AccountController.java
@@ -0,0 +1,62 @@
+package com.jeczzu.fintechapi.controller;
+
+import java.net.URI;
+import java.util.UUID;
+
+import org.springframework.http.ResponseEntity;
+
+import com.jeczzu.fintechapi.dto.AccountResponse;
+import com.jeczzu.fintechapi.dto.CreateAccountRequest;
+import com.jeczzu.fintechapi.entity.Account;
+import com.jeczzu.fintechapi.service.AccountService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/accounts")
+@RequiredArgsConstructor
+public class AccountController {
+
+  private final AccountService accountService;
+
+  @PostMapping
+  public ResponseEntity<AccountResponse> createAccount(
+      @Valid @RequestBody CreateAccountRequest request) {
+
+    Account account = accountService.createAccount(
+        request.ownerName(),
+        request.email());
+
+    AccountResponse response = new AccountResponse(
+        account.getId(),
+        account.getOwnerName(),
+        account.getEmail(),
+        account.getBalance(),
+        account.getCreatedAt());
+
+    return ResponseEntity
+        .created(URI.create("/api/accounts/" + account.getId()))
+        .body(response);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<AccountResponse> getAccount(@PathVariable UUID id) {
+
+    Account account = accountService.getAccountById(id);
+
+    AccountResponse response = new AccountResponse(
+      account.getId(),
+      account.getOwnerName(),
+      account.getEmail(),
+      account.getBalance(),
+      account.getCreatedAt()
+    );
+
+    return ResponseEntity.ok(response);
+  }
+  
+}
