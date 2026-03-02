7dee10da07d2a4cc69c67f0ba87821e1bf23ccbd
JeczzuDev <jesusignaciocc22@gmail.com>
2026-03-02 20:56:18 -0300

    feat(routes): Add ApiRoutes class for defining API endpoint paths

---
+++ src/main/java/com/jeczzu/fintechapi/config/ApiRoutes.java
@@ -0,0 +1,16 @@
+package com.jeczzu.fintechapi.config;
+
+public final class ApiRoutes {
+  
+  private ApiRoutes() {
+  }
+
+  public static final String API_BASE = "/api";
+
+  // AccountController
+  public static final String ACCOUNTS = API_BASE + "/accounts";
+  public static final String ACCOUNT_BY_ID = "/{id}";
+
+  // TransactionController
+  public static final String TRANSACTIONS = API_BASE + "/transactions";
+}
