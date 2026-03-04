c69394b92d576fe8fed451957b6811e63e05bc1b
JeczzuDev <jesusignaciocc22@gmail.com>
2026-03-03 23:06:28 -0300

    refactor(TransactionController): Update request mapping to use ApiRoutes constants

--- src/main/java/com/jeczzu/fintechapi/controller/TransactionController.java
+++ src/main/java/com/jeczzu/fintechapi/controller/TransactionController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jeczzu.fintechapi.config.ApiRoutes;
 import com.jeczzu.fintechapi.dto.CreateTransactionRequest;
 import com.jeczzu.fintechapi.dto.TransactionResponse;
 import com.jeczzu.fintechapi.entity.Transaction;
@@ -24,7 +25,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/accounts/{accountId}/transactions")
+@RequestMapping(ApiRoutes.TRANSACTIONS)
 @RequiredArgsConstructor
 public class TransactionController {
 
@@ -41,7 +42,7 @@ public class TransactionController {
     TransactionResponse response = TransactionMapper.toResponse(transaction);
 
     URI location = URI.create(
-        "/api/accounts/" + accountId + "/transactions/" + transaction.getId());
+        ApiRoutes.ACCOUNTS + "/" + accountId + "/transactions/" + transaction.getId());
     return ResponseEntity.created(location).body(response);
   }
 
